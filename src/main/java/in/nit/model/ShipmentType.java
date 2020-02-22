ba5fde099a2384e8fcccb6caca46b7f6ebd5d628
abhay <abhay.my.786@gmail.com>
2020-02-22 13:04:26 +0530

    ShiomentType-5555-Add setter/getter,def constructor and toString

--- src/main/java/in/nit/model/ShipmentType.java
+++ src/main/java/in/nit/model/ShipmentType.java
@@ -22,5 +22,66 @@ public class ShipmentType {
 	private String shipGrade;
 	@Column(name="sdesc")
 	private String shipDesc;
+	
+	public ShipmentType() {
+		super();
+	}
+
+	public Integer getShipId() {
+		return shipId;
+	}
+
+	public void setShipId(Integer shipId) {
+		this.shipId = shipId;
+	}
+
+	public String getShipMode() {
+		return shipMode;
+	}
+
+	public void setShipMode(String shipMode) {
+		this.shipMode = shipMode;
+	}
+
+	public String getShipCode() {
+		return shipCode;
+	}
+
+	public void setShipCode(String shipCode) {
+		this.shipCode = shipCode;
+	}
+
+	public String getEnabShip() {
+		return enabShip;
+	}
+
+	public void setEnabShip(String enabShip) {
+		this.enabShip = enabShip;
+	}
+
+	public String getShipGrade() {
+		return shipGrade;
+	}
+
+	public void setShipGrade(String shipGrade) {
+		this.shipGrade = shipGrade;
+	}
+
+	public String getShipDesc() {
+		return shipDesc;
+	}
+
+	public void setShipDesc(String shipDesc) {
+		this.shipDesc = shipDesc;
+	}
+
+	@Override
+	public String toString() {
+		return "ShipmentType [shipId=" + shipId + ", shipMode=" + shipMode + ", shipCode=" + shipCode + ", enabShip="
+				+ enabShip + ", shipGrade=" + shipGrade + ", shipDesc=" + shipDesc + "]";
+	}
+	
+	
+	
 
 }
